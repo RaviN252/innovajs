e87aa2bde2042a6fed60fae514a4d9cd2dda8aa1
Ravi N <94108095+RaviN252@users.noreply.github.com>
2024-06-14 14:42:32 +0530

    second commit

---
+++ script-kidde/app.js
@@ -0,0 +1,17 @@
+console.time("code");
+console.log("hello World");
+console.log("Subscribe to my youtube channel");
+
+console.warn("This is warning");
+console.error("TShis is an error");
+// console.clear();
+/* creating an array in a table */
+console.table([1, 2, 3, 4, 5, 6]);
+/* creating an object in a table */
+console.table({ num1: "1", num2: "2", num3: "3", num4: "4", num4: "4" });
+// console.clear();
+
+/* assertion function it is similar to if else a type not exactly */
+console.assert(1 > 2, "It is not possible ");
+/* console.time and console.timeEnd will calaculate the speed of the program*/
+console.timeEnd("code");
